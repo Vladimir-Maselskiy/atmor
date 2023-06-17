68f14e5279a2d3a73f142e224f26c1f89606c2fc
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-06-17 23:09:00 +0300

    feat: add item order content

---
+++ src/components/OrderItem/OrderItem.styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledOrderItem = styled.li`
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export const ModelName = styled.p`
+  font-weight: 700;
+`;
+
+export const StyledPrice = styled.p`
+  font-size: 32px;
+  color: var(--accent-color);
+  white-space: nowrap;
+`;
--- src/components/OrderItem/OrderItem.tsx
+++ src/components/OrderItem/OrderItem.tsx
@@ -1,10 +1,49 @@
 import { ICartItem } from '@/interfaces/interfaces';
+import { Divider, Image } from 'antd';
 import React from 'react';
+import { Box } from '../Box/Box';
+import { ModelName, StyledOrderItem, StyledPrice } from './OrderItem.styled';
+import { getPriceSpacesFormatted } from '@/utils/getPriceSpacesFormatted';
 
 type TProps = {
   item: ICartItem;
 };
 
 export const OrderItem = ({ item }: TProps) => {
-  return <div>OrderItem</div>;
+  const {
+    product: { aditional, options },
+    quantity,
+  } = item;
+  return (
+    <StyledOrderItem>
+      <Box position="relative" display="flex" width="100%" paddingRight={60}>
+        <Box>
+          <Image
+            src={options.firstPhoto}
+            alt={`${aditional.model} photo`}
+            width={200}
+            height={256}
+            style={{ objectFit: 'contain', objectPosition: 'center' }}
+          />
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          flexGrow={1}
+          lineHeight={1.5}
+          paddingTop={40}
+          width={500}
+        >
+          <Box marginLeft={116}>
+            <ModelName>{aditional.model}</ModelName>
+            <p>{options.name}</p>
+          </Box>
+          <StyledPrice>
+            {getPriceSpacesFormatted(options.price * quantity)} грн
+          </StyledPrice>
+        </Box>
+      </Box>
+      <Divider />
+    </StyledOrderItem>
+  );
 };
