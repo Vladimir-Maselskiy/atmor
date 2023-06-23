6665ff630fc06ae8b5f5967745a3765072fa7bc8
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-06-23 14:47:45 +0300

    feat: swiper navigation for product

--- src/components/Product/Product.styled.ts
+++ src/components/Product/Product.styled.ts
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
 
-export const PreviewPhotosList = styled.ul`
+type TProps = {
+  photoQuantity: number;
+};
+
+export const PreviewPhotosList = styled.ul<TProps>`
   display: flex;
+  justify-content: space-between;
+  margin-top: 40px;
+  width: ${p => `${100 * p.photoQuantity + (p.photoQuantity - 1) * 40}px`};
+  margin-inline: auto;
 `;
 export const PreviewPhotosItem = styled.li`
+  position: relative;
+  width: 100px;
+  aspect-ratio: 0.85;
 `;
-
-
--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,19 +1,12 @@
 import { IAtmorItem } from '@/interfaces/interfaces';
 import React, { useState } from 'react';
 import { Box } from '../Box/Box';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
-import {
-  Autoplay,
-  Pagination,
-  Navigation,
-  EffectFade,
-  FreeMode,
-  Thumbs,
-} from 'swiper';
+import { Thumbs } from 'swiper';
 import Image from 'next/image';
 import { PreviewPhotosItem, PreviewPhotosList } from './Product.styled';
 
@@ -22,46 +15,58 @@ type TProps = {
 };
 
 export const Product = ({ product }: TProps) => {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
   return (
     <Box paddingTop={115}>
-      <Box width="50%">
+      <Box width="520px" p={16}>
         <Swiper
           style={{
-            aspectRatio: 2 / 1,
+            aspectRatio: 1.5,
           }}
           thumbs={{ swiper: thumbsSwiper }}
           // effect={'fade'}
           //   spaceBetween={30}
           centeredSlides={true}
-          // modules={[
-          //   EffectFade,
-          //   Autoplay,
-          //   Pagination,
-          //   Navigation,
-          //   FreeMode,
-          //   Thumbs,
-          // ]}
+          modules={[Thumbs]}
           className="productSwiper"
         >
           {product.aditional.photos.map(photo => (
             <SwiperSlide key={photo}>
-              <Image src={`${photo}`} alt={`${product.options.name}`} fill />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <PreviewPhotosList>
-          {product.aditional.photos.map(photo => (
-            <PreviewPhotosItem key={photo}>
               <Image
                 src={`${photo}`}
                 alt={`${product.options.name}`}
-                width={100}
-                height={100}
+                fill
+                objectFit="contain"
               />
-            </PreviewPhotosItem>
+            </SwiperSlide>
           ))}
-        </PreviewPhotosList>
+        </Swiper>
+        <Box px={20} mt={20}>
+          <Swiper
+            style={{
+              height: 100,
+            }}
+            spaceBetween={50}
+            slidesPerView={3}
+            modules={[Thumbs]}
+            watchSlidesProgress
+            onSwiper={setThumbsSwiper}
+            className="swiper-for-navigation"
+          >
+            {product.aditional.photos.map(photo => (
+              <SwiperSlide key={photo} style={{ padding: 10 }}>
+                <Box position="relative" height="100%">
+                  <Image
+                    src={`${photo}`}
+                    alt={`${product.options.name}`}
+                    fill
+                    objectFit="contain"
+                  />
+                </Box>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Box>
       </Box>
     </Box>
   );
