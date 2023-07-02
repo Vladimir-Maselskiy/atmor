57a2a92e1664afdb81d3b30f265883202af6ce00
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-07-02 20:51:44 +0300

    fix: env names

--- src/app/api/novaposhta/searchWarehouses/route.ts
+++ src/app/api/novaposhta/searchWarehouses/route.ts
@@ -1,14 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import {  NextApiResponse } from 'next';
 import { NextResponse, NextRequest } from 'next/server';
-const url = process.env.NEXT_PUBLIC_API_HOST!;
 
 export const POST = async (req: NextRequest, res: NextApiResponse) => {
   const { cityRef, findByString = '' } = await req.json();
 
-  const url = process.env.NEXT_PUBLIC_NOVA_POSHTA_URL!;
+  const url = process.env.NOVA_POSHTA_URL!;
 
   const body = {
-    apiKey: process.env.NEXT_PUBLIC_NOVA_POSHTA_API_KEY,
+    apiKey: process.env.NOVA_POSHTA_API_KEY,
     modelName: 'Address',
     calledMethod: 'getWarehouses',
     methodProperties: {
