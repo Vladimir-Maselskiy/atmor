d0b5da6c186d5f634729914636f6a272719c71a4
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-07-31 14:32:52 +0300

    fix: cors4, add proxi api

--- src/app/api/telegram/sendMessage/route.ts
+++ src/app/api/telegram/sendMessage/route.ts
@@ -3,19 +3,21 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export const POST = async (req: NextRequest, res: NextApiResponse) => {
   const body = await req.json();
+  console.log('body', body);
 
   const url = process.env.TELEGRAM_BOT_URL!;
 
   const options = {
     method: 'POST',
-    body,
+    body: JSON.stringify(body),
   };
 
   try {
-    await fetch(url, options).then(res => res.json());
+    const result = await fetch(url, options).then(res => res.json());
+    console.log('res in sendMessage', result);
     return NextResponse.json({ Status: 200 });
   } catch (error) {
-    console.log(error);
+    console.log('error in sendMessage', error);
     return NextResponse.error();
   }
 };
